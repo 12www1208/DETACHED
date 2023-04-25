a6ef48f0264af2a0262846f551a7133c2e1e6b34
Karter_Kein <MAKCNM_0709@mail.ru>
2023-04-25 22:09:06 +0300

    create component navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-  
+import Navbar from './components/navbar/navbar.vue';
+ 
 </script>
 
 <template>
-
+  <Navbar/>
 </template>
---
+++ src/components/navbar/navbar.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <nav class="h-16 bg-black flex w-full items-center fixed z-50 justify-center" >
+    <ul>
+      <li class="menu-item"><a href="#">Главная</a></li>
+      <li class="menu-item"><a href="#">Наш состав</a></li>
+      <li class="menu-item"><a href="#">Новости</a></li>
+      <li class="menu-item"><a href="#">Клипы</a></li>
+    </ul>
+  </nav>
+</template>
