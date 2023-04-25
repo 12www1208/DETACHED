9913d877fd8e8467e6687ab1d0f357139f7ec995
Karter_Kein <MAKCNM_0709@mail.ru>
2023-04-25 19:22:28 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue, js}'],
+  theme: {
+    extend: {
+      colors: {
+        'd22': '#252525',
+        'hE': '#E4D0D0'
+      }
+    },
+  },
+  plugins: [],
+}
+
