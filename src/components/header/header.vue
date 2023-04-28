ab998b3ce763c06f3e5d5478e463c8c58b8224fe
Karter_Kein <MAKCNM_0709@mail.ru>
2023-04-28 22:57:19 +0300

    hotfix

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -5,9 +5,9 @@ import logo from '../../assets/img/logo.jpg'
 
 <template>
   <header class="flex text-white  h-screen justify-evenly items-center flex-col font-Rubik">
-    <img class="md:px-4  md:w-96" :src="logo" alt="">
+    <img class=" px-4  md:w-96" :src="logo" alt="">
     <h1 class='text-3xl'>DEТACHED</h1>
-    <p class=" text-xl md:text-2xl w-full text-center" >DETACHED - Рок-группа, музыкальный проект, авторами которого являются гитарист композитор The Sinner и вокалист Mephistopheles. Проект занимается выпуском тяжёлой, а так же разножанровой музыки, съëмками видеоклипов, а так же профессиональными фотосессиями. </p>
+    <p class=" text-md md:text-2xl w-full text-center" >DETACHED - Рок-группа, музыкальный проект, авторами которого являются гитарист композитор The Sinner и вокалист Mephistopheles. Проект занимается выпуском тяжёлой, а так же разножанровой музыки, съëмками видеоклипов, а так же профессиональными фотосессиями. </p>
   </header>
 </template>
 
