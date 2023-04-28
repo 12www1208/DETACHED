59ed7fadf98648858ab194f573d6ed046fd21443
Karter_Kein <MAKCNM_0709@mail.ru>
2023-04-28 22:26:54 +0300

    create page

--- src/components/ui/preloader/preloader.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-  
-</script>
-
-<template>
-  <div class="preloader  flex w-full h-full bg-d22 items-center justify-center z-50 fixed">
-    <h1 class="font-Rubik text-white text-center text-3xl md:text-4xl">Пожалуйста подождите</h1>
-  </div>
-</template>
